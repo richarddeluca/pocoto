84d3aed2a850d11ae6b33dcf73060a7ad376e78c
Richard Deluca <richard.deluca.dev@gmail.com>
2022-01-28 01:06:53 -0300

    adicionando favicon

--- pages/_document.jsx
+++ pages/_document.jsx
@@ -10,7 +10,8 @@ class MyDocument extends Document {
           <link rel="preconnect" href="https://fonts.googleapis.com"/>
           <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true"/>
           <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@400;600;700&display=swap" rel="stylesheet"/>
-          <link rel="icon" type="image/png" href="https://www.flaticon.com/free-icons/witch" />
+          {/* <link rel="icon" href="/" /> */}
+          <link rel="shortcut icon" href="https://raw.githubusercontent.com/richarddeluca/site-rpg/main/public/static/icon/favicon.ico" />
           <meta name="description" content="Mundos, narrativas e personagens dos jogos RPG que participei na UnB" />
         </Head>
         <body>
